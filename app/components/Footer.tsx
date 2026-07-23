7ca7191cbdc5a081519c018372976e80e0508487
Sean Currlin <sean.currlin@gmail.com>
2026-07-23 17:27:45 -0400

    version 1.97

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-white px-5 py-10 text-black sm:px-8">
       <div className="mx-auto flex max-w-7xl justify-center">
-        <p className="text-base font-semibold tracking-wide sm:text-lg">
+        <p className="text-lg font-semibold tracking-wide sm:text-xl">
           &copy; {year} SEΛN X KΛLO
         </p>
       </div>
